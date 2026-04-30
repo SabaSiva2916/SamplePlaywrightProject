117bcd8fb08e54a920e09703406736fed0c86f21
SabaSiva2916 <sabasiva2916@gmail.com>
2026-04-30 11:02:42 +0530

    New cooded added

--- Sample.js
+++ Sample.js
@@ -19,3 +19,15 @@ arr.forEach(function (element) {
 //Finding the index of an element
 let index = arr.indexOf(2.5);
 console.log(index); // Output: 3
+
+//Removing an element from the end of the array
+arr.pop();
+console.log(arr); // Output: [0, 1, 2, 2.5, 3, 4, 5]
+
+//Removing an element from the beginning of the array
+arr.shift();
+console.log(arr); // Output: [1, 2, 2.5, 3, 4, 5]
+
+//Removing an element from a specific index
+arr.splice(2, 1);
+console.log(arr); // Output: [1, 2, 3, 4, 5]c
