83c31ad58b6e1589da835bcd48c72d40e5815baa
SabaSiva2916 <sabasiva2916@gmail.com>
2026-05-05 09:57:43 +0530

    Update Reverse code

--- Sample.js
+++ Sample.js
@@ -19,3 +19,20 @@ arr.forEach(function (element) {
 //Finding the index of an element
 let index = arr.indexOf(2.5);
 console.log(index); // Output: 3
+
+//modification in code
+for (a of arr) {
+  console.log(a);
+}
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] > arr[j]) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+}
+
+console.log(arr); // Output: [0, 1, 2, 2.5, 3, 4, 5, 6] (sorted array)
